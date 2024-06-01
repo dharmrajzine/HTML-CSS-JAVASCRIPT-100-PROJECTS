d744bd4471958264c612048b3a1945173978c1ea
dharmrajzine <dharmrajzine@gmail.com>
2024-06-01 23:43:18 +0530

    Feedback UI using HTML CSS JS

---
+++ 84. Feedback UI/index.js
@@ -0,0 +1,31 @@
+const ratingElements = document.querySelectorAll(".rating");
+const btnElement = document.getElementById("btn");
+
+const containerElement = document.getElementById("container");
+let selectedRating = "";
+
+ratingElements.forEach((ratingEl) => {
+    ratingEl.addEventListener("click", (event) => {
+        removeActive();
+        selectedRating = 
+        event.target.innerText || event.target.parentNode.innerText; 
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+    });
+});
+
+btnElement.addEventListener("click",()=> {
+    if(selectedRating !== ""){
+        containerElement.innerHTML = `
+        <strong> Thank you!</strong>
+        <br><br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+        `
+    }
+})
+function removeActive(){
+    ratingElements.forEach((ratingEl) => {
+        ratingEl.classList.remove("active");
+    });
+}
